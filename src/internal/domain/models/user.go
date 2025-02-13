45cba23795f6e536589158ed67fdfd28940afeb5
snickyyy <leon12345671234@gmail.com>
2025-02-13 14:28:30 +0100

    change fields in User model

--- src/internal/domain/models/user.go
+++ src/internal/domain/models/user.go
@@ -2,21 +2,21 @@ package domain
 
 type User struct {
 	BaseModel
-	username 	string	`gorm:"unique;size:40;not null;"`
-	email 		string	`gorm:"unique;size:255;not null;"`
-	password 	string	`gorm:"not null"`
-	description string	`gorm:"size:255;"`
-	role 		string	`gorm:"size:50;not null;default:'anonymous'"`
-	image 		string
+	Username 	string	`gorm:"unique;size:40;not null;"`
+	Email 		string	`gorm:"unique;size:255;not null;"`
+	Password 	string	`gorm:"not null"`
+	Description string	`gorm:"size:255;"`
+	Role 		string	`gorm:"size:50;not null;default:'anonymous'"`
+	Image 		string
 }
 
 func NewUser(username, email, password, description, role, image string) *User {
 	return &User{
-		username: username,
-		email: email,
-		password: password,
-		description: description,
-		role: role,
-		image: image,
+		Username: username,
+		Email: email,
+		Password: password,
+		Description: description,
+		Role: role,
+		Image: image,
 	}
 }
