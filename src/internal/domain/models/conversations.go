f82bdba7faf17af1001b522f94dc1d4cb97b6c8f
snickyyy <leon12345671234@gmail.com>
2025-02-13 17:59:46 +0100

    add messages schema

--- src/internal/domain/models/conversations.go
+++ src/internal/domain/models/conversations.go
@@ -8,3 +8,13 @@ type Chat struct {
 	Description	string 	`bson:"description" json:"description"`
 	Members		[]int64 `bson:"members"`
 }
+
+type Message struct {
+	BaseMongo
+	SenderId 	string	`bson:"sender_id" json:"sender_id"`
+	ChatId		int64   `bson:"chat_id" json:"chat_id"`
+	MessageType	string 	`bson:"message_type" json:"message_type"`
+	Content     string  `bson:"content" json:"content"`
+	IsUpdated	bool 	`bson:"is_updated" json:"is_updated"`
+	IsDeleted	bool `bson:"is_deleted" json:"is_deleted"`
+}
