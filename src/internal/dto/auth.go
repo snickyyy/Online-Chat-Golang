86b13f9212d11f33dbfd08fbd9b4b2aceda23291
snickyyy <leon12345671234@gmail.com>
2025-02-23 17:02:53 +0100

    RegisterRequest schema

---
+++ src/internal/dto/auth.go
@@ -0,0 +1,8 @@
+package dto
+
+type RegisterRequest struct {
+	Username 		string 	`json:"username" binding:"required,min=4,max=28"`
+	Email       	string  `json:"email" binding:"required,email"`
+	Password    	string  `json:"password" binding:"required"`
+	ConfirmPassword string 	`json:"confirm_password" binding:"required"`
+}
