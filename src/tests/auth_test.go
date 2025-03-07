eceb314695836e11e34c6fbb293ff3bc89a9e12e
snickyyy <leon12345671234@gmail.com>
2025-03-07 22:30:08 +0100

    add login test

--- src/tests/auth_test.go
+++ src/tests/auth_test.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"libs/src/internal/dto"
+	services "libs/src/internal/usecase"
 	api_errors "libs/src/internal/usecase/errors"
+	"libs/src/settings"
 	"log"
 	"net/http"
 )
@@ -50,4 +52,38 @@ func (suite *AppTestSuite) TestRegister() {
 	suite.Equal(http.StatusBadRequest, res.StatusCode)
 	log.Println("Response status code: ", res.StatusCode)
 
+}
+
+func (suite *AppTestSuite) TestLogin() {
+	log.Println("Testing Login endpoint...")
+    url := "http://127.0.0.1:8000/accounts/auth/login"
+    contType := "application/json"
+
+	err := services.NewUserService(settings.AppVar).CreateSuperUser("testuser", "test@test.com", "test123")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    dataSuccess, _ := json.Marshal(dto.LoginRequest{
+        UsernameOrEmail: "testuser",
+        Password: "test123",
+    })
+
+    res, err := suite.client.Post(url, contType, bytes.NewBuffer(dataSuccess))
+    suite.NoError(err)
+    suite.Equal(http.StatusOK, res.StatusCode)
+	suite.NotEmpty(res.Cookies())
+    log.Println("Response status code: ", res.StatusCode)
+
+    dataFail, _ := json.Marshal(dto.LoginRequest{
+        UsernameOrEmail: "testuser",
+        Password: "false_password",
+    })
+    res, err = suite.client.Post(url, contType, bytes.NewBuffer(dataFail))
+    suite.NoError(err)
+	suite.Equal(http.StatusConflict, res.StatusCode)
+}
+
+func (suite *AppTestSuite) TestLogout() {
+	// TODO: Make test logout
 }
