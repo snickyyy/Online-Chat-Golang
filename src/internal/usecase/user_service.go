6c62b717ff132818d17690b21d8fde211f22f69e
snickyyy <leon12345671234@gmail.com>
2025-03-07 20:30:49 +0100

    add UserService

--- src/internal/usecase/user_service.go
+++ src/internal/usecase/user_service.go
@@ -1,5 +1,24 @@
 package services
 
+import (
+	domain "libs/src/internal/domain/models"
+	"libs/src/internal/repositories"
+	"libs/src/settings"
+)
+
 type UserService struct {
-	
+	App 				*settings.App
+	UserRepository 		*repositories.UserRepository
+}
+
+func NewUserService(app *settings.App) *UserService {
+	return &UserService{
+		App: app,
+		UserRepository: &repositories.UserRepository{
+			BasePostgresRepository: repositories.BasePostgresRepository[domain.User]{
+				Model: domain.User{},
+				Db:    app.DB,
+			},
+		},
+	}
 }
