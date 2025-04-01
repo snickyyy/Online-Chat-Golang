aee1c348ac2ab57a73d80017e1134f5a03708de8
snickyyy <leon12345671234@gmail.com>
2025-04-01 22:38:32 +0200

    add TestCreateChatDuplicate and TestCreateChatSuccess

--- src/tests/unit/chat_service_test.go
+++ src/tests/unit/chat_service_test.go
@@ -6,6 +6,7 @@ import (
 	"libs/src/internal/domain/enums"
 	"libs/src/internal/dto"
 	"libs/src/internal/mocks"
+	"libs/src/internal/repositories"
 	services "libs/src/internal/usecase"
 	api_errors "libs/src/internal/usecase/errors"
 	"testing"
@@ -57,4 +58,45 @@ func TestCreateChatUnauthorized(t *testing.T) {
 		_, err := chatService.CreateChat(request, tc.user)
 		assert.Equal(t, err, tc.error, "I: %d", i)
 	}
+}
+func TestCreateChatDuplicate(t *testing.T) {
+	mockApp := GetAppMock()
+	MockChatRepository := new(mocks.IChatRepository)
+
+	MockChatRepository.EXPECT().Create(mock.Anything).Return(repositories.ErrDuplicate)
+	chatService := services.ChatService{App: mockApp, ChatRepository: MockChatRepository}
+
+	request := dto.CreateChatRequest{
+		Title:       "Test Chat",
+		Description: "Test Description",
+	}
+	user := dto.UserDTO{
+		ID:       1,
+		Role:     enums.USER,
+		IsActive: true,
+	}
+	_, err := chatService.CreateChat(request, user)
+	assert.Equal(t, err, api_errors.ErrChatAlreadyExists)
+}
+func TestCreateChatSuccess(t *testing.T) {
+	mockApp := GetAppMock()
+	MockChatRepository := new(mocks.IChatRepository)
+
+	MockChatRepository.EXPECT().Create(mock.Anything).Return(nil)
+	chatService := services.ChatService{App: mockApp, ChatRepository: MockChatRepository}
+
+	request := dto.CreateChatRequest{
+		Title:       "Test Chat",
+		Description: "Test Description",
+	}
+	user := dto.UserDTO{
+		ID:       1,
+		Role:     enums.USER,
+		IsActive: true,
+	}
+	chat, err := chatService.CreateChat(request, user)
+	assert.NoError(t, err)
+	assert.Equal(t, chat.Title, request.Title)
+	assert.Equal(t, chat.Description, request.Description)
+	assert.Equal(t, chat.OwnerID, user.ID)
 }
