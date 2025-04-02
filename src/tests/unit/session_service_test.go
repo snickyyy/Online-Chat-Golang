97ef05a9a34d3d9604babfd6bcaa563e297d4e56
snickyyy <leon12345671234@gmail.com>
2025-04-02 23:40:02 +0200

    add TestDeleteSession

--- src/tests/unit/session_service_test.go
+++ src/tests/unit/session_service_test.go
@@ -50,3 +50,42 @@ func TestGetSession(t *testing.T) {
 		})
 	}
 }
+func TestDeleteSession(t *testing.T) {
+	mockApp := GetAppMock()
+	sessionService := services.SessionService{
+		App: mockApp,
+	}
+
+	testCases := []struct {
+		testName    string
+		Prefix      string
+		Session     string
+		mockResp    int64
+		mockErr     error
+		mustRespErr error
+		mustErr     bool
+	}{
+		{
+			"TestDeleteSessionInvalidSession", "test", "test", 0, errors.New("Invalid session"), api_errors.ErrInvalidSession, true,
+		},
+		{
+			"TestDeleteSessionValidSession", "test", "test", 1, nil, nil, false,
+		},
+	}
+	for _, tc := range testCases {
+		mockRedisRepo := new(mocks.IBaseRedisRepository)
+		sessionService.RedisBaseRepository = mockRedisRepo
+
+		t.Run(tc.testName, func(t *testing.T) {
+			mockRedisRepo.EXPECT().Delete(tc.Prefix, tc.Session).Return(tc.mockResp, tc.mockErr)
+
+			err := sessionService.DeleteSession(tc.Prefix, tc.Session)
+			if tc.mustErr {
+				assert.Error(t, err)
+				assert.Equal(t, tc.mustRespErr, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
