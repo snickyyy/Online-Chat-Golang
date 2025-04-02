3a9770e87967f0cecced37a1b04f75d427f4278e
snickyyy <leon12345671234@gmail.com>
2025-04-02 23:19:02 +0200

    add TestGetSession

---
+++ src/tests/unit/session_service_test.go
@@ -0,0 +1,52 @@
+package unit
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"libs/src/internal/dto"
+	"libs/src/internal/mocks"
+	services "libs/src/internal/usecase"
+	api_errors "libs/src/internal/usecase/errors"
+	"testing"
+)
+
+func TestGetSession(t *testing.T) {
+	mockApp := GetAppMock()
+
+	sessionService := services.SessionService{
+		App: mockApp,
+	}
+
+	testCases := []struct {
+		testName    string
+		Prefix      string
+		Session     string
+		mockResp    string
+		mockErr     error
+		mustResp    dto.SessionDTO
+		mustRespErr error
+		mustErr     bool
+	}{
+		{"TestGetSessionInvalidSession", "test", "test", "", errors.New("Invalid session"), dto.SessionDTO{}, api_errors.ErrInvalidSession, true},
+		{"TestGetSessionValidSession", "test", "test", `{"id":"test","prefix":"test","payload":"test"}`, nil, dto.SessionDTO{SessionID: "test", Prefix: "test"}, nil, false},
+	}
+	for _, tc := range testCases {
+		mockRedisRepo := new(mocks.IBaseRedisRepository)
+		sessionService.RedisBaseRepository = mockRedisRepo
+
+		t.Run(tc.testName, func(t *testing.T) {
+			mockRedisRepo.EXPECT().GetByKey(tc.Prefix, tc.Session).Return(tc.mockResp, tc.mockErr)
+
+			resp, err := sessionService.GetSession(tc.Prefix, tc.Session)
+			if tc.mustErr {
+				assert.Error(t, err)
+				assert.Equal(t, tc.mustRespErr, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.mustResp.SessionID, resp.SessionID)
+				assert.Equal(t, tc.mustResp.Prefix, resp.Prefix)
+			}
+
+		})
+	}
+}
