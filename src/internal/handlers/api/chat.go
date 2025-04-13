e6a7a511e45d6450394fff187f9a930de3792610
snickyyy <leon12345671234@gmail.com>
2025-04-13 17:41:35 +0200

    divide chat on chat and chat_member

--- src/internal/handlers/api/chat.go
+++ src/internal/handlers/api/chat.go
@@ -41,44 +41,6 @@ func CreateChat(c *gin.Context) {
 	c.JSON(http.StatusOK, chat)
 }
 
-// @Summary Invite to chat
-// @Description inviting a user to an existing chat
-// @Tags Chat
-// @Accept json
-// @Produce json
-// @Param invitee query string true "Invitee username"
-// @Param chat_id query int true "chat id to invite to"
-// @Success 200 {object} dto.MessageResponse
-// @Failure 400 {object} dto.ErrorResponse
-// @Failure 500 {object} dto.ErrorResponse
-// @Router /messenger/chat/invite [post]
-func InviteToChat(c *gin.Context) {
-	app := c.MustGet("app").(*settings.App)
-	inviter := c.MustGet("user").(dto.UserDTO)
-
-	invitee := c.Query("invitee")
-	chatID := c.Query("chat_id")
-	if invitee == "" || chatID == "" {
-		c.Error(api_errors.ErrInvalidData)
-		return
-	}
-
-	service := services.NewChatMemberService(app)
-
-	chatIDInt, err := strconv.Atoi(chatID)
-	if err != nil {
-		c.Error(api_errors.ErrInvalidData)
-		return
-	}
-
-	err = service.InviteToChat(&inviter, invitee, int64(chatIDInt))
-	if err != nil {
-		c.Error(err)
-		return
-	}
-	c.JSON(http.StatusOK, dto.MessageResponse{Message: "success"})
-}
-
 // @Summary Delete chat
 // @Description deleting a chat
 // @Tags Chat
