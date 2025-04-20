f5fd25637ea4e6569172b5e65b5199c4bfca1853
snickyyy <leon12345671234@gmail.com>
2025-04-20 19:15:39 +0200

    add custom errors for usecase_errors.go

--- src/internal/errors/usecase_errors.go
+++ src/internal/errors/usecase_errors.go
@@ -19,3 +19,58 @@ type IUnauthorizedError interface {
 type IBadRequestError interface {
 	BadRequestError() string
 }
+
+type NotFoundError struct {
+	BaseError
+}
+
+func (e *NotFoundError) NotFountError() string {
+	return e.Msg
+}
+func (e *NotFoundError) Error() string {
+	return e.Msg
+}
+
+type PermissionError struct {
+	BaseError
+}
+
+func (e *PermissionError) PermissionError() string {
+	return e.Msg
+}
+func (e *PermissionError) Error() string {
+	return e.Msg
+}
+
+type AlreadyExistsError struct {
+	BaseError
+}
+
+func (e *AlreadyExistsError) AlreadyExistsError() string {
+	return e.Msg
+}
+func (e *AlreadyExistsError) Error() string {
+	return e.Msg
+}
+
+type UnauthorizedError struct {
+	BaseError
+}
+
+func (e *UnauthorizedError) UnauthorizedError() string {
+	return e.Msg
+}
+func (e *UnauthorizedError) Error() string {
+	return e.Msg
+}
+
+type BadRequestError struct {
+	BaseError
+}
+
+func (e *BadRequestError) BadRequestError() string {
+	return e.Msg
+}
+func (e *BadRequestError) Error() string {
+	return e.Msg
+}
