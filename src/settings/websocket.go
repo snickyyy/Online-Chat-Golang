42f212aa1d9ce21cff46e2da54dbfb75860bfedd
snickyyy <leon12345671234@gmail.com>
2025-05-08 17:31:00 +0200

    add di for upgrader

---
+++ src/settings/websocket.go
@@ -0,0 +1,14 @@
+package settings
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+func NewUpgrader() *websocket.Upgrader {
+	return &websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+}
