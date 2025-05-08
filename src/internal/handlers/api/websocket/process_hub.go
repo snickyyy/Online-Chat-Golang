a5821070124e3194b0a14a75039a47bd94df5c16
snickyyy <leon12345671234@gmail.com>
2025-05-08 17:30:05 +0200

    fix websockets Models and add process_hub.go

---
+++ src/internal/handlers/api/websocket/process_hub.go
@@ -0,0 +1,54 @@
+package websocket
+
+import (
+	"encoding/json"
+	"go.uber.org/zap"
+	"libs/src/internal/infrastructure"
+	services "libs/src/internal/usecase"
+)
+
+func RunProcessHub(hub *infrastructure.WebSocketHub) {
+	msgService := services.NewMessageService(hub.App)
+
+	for {
+		select {
+
+		case client := <-hub.Add:
+			hub.Mx.Lock()
+			hub.Clients[client] = true
+			hub.Mx.Unlock()
+
+		case client := <-hub.Delete:
+			hub.Mx.Lock()
+			delete(hub.Clients, client)
+			hub.Mx.Unlock()
+
+		case subscribe := <-hub.Subscribe:
+			subscribe.Client.Mx.Lock()
+			subscribe.Client.Subscriptions[subscribe.ChatId] = true
+			subscribe.Client.Mx.Unlock()
+
+		case unsubscribe := <-hub.Unsubscribe:
+			unsubscribe.Client.Mx.Lock()
+			delete(unsubscribe.Client.Subscriptions, unsubscribe.ChatId)
+			unsubscribe.Client.Mx.Unlock()
+
+		case messages := <-hub.Messagebox:
+			for _, msg := range messages {
+				messagePreview, err := msgService.NewMessage(hub.App.Ctx, msg.From.UserDto, string(msg.Data), msg.ToChat)
+				if err != nil {
+					hub.App.Logger.Error("new message failed", zap.Error(err))
+					continue
+				}
+
+				messagePreviewToJson, _ := json.Marshal(messagePreview)
+
+				for k, _ := range hub.Clients {
+					if k.Subscriptions[msg.ToChat] {
+						k.Send(messagePreviewToJson)
+					}
+				}
+			}
+		}
+	}
+}
--- src/internal/infrastructure/websocket.go
+++ src/internal/infrastructure/websocket.go
@@ -1,25 +1,66 @@
 package infrastructure
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"libs/src/internal/dto"
+	"libs/src/settings"
+	"sync"
 )
 
 type WebSocketClient struct {
-	UserDto    *dto.UserDTO
-	Conn       *websocket.Conn
-	Messagebox chan []byte
+	Mx            *sync.RWMutex
+	UserDto       *dto.UserDTO
+	Subscriptions map[int64]bool
+	Conn          *websocket.Conn
+	Messagebox    chan []byte
+}
+
+func NewWebSocketClient(conn *websocket.Conn, user *dto.UserDTO) *WebSocketClient {
+	return &WebSocketClient{
+		UserDto:    user,
+		Conn:       conn,
+		Messagebox: make(chan []byte),
+	}
+}
+
+func (c *WebSocketClient) Send(message []byte) {
+	if err := c.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
+		settings.AppVar.Logger.Error(fmt.Sprintf("websocket send message error %v", err))
+	}
 }
 
 type Message struct {
-	From *WebSocketClient
-	To   *WebSocketClient
-	Data []byte
+	From   *WebSocketClient
+	ToChat int64
+	Data   []byte
 }
 
 type WebSocketHub struct {
-	Clients    map[*WebSocketClient]bool
-	Messagebox chan []Message
-	Add        chan *WebSocketClient
-	Delete     chan *WebSocketClient
+	App         *settings.App
+	Mx          *sync.RWMutex
+	Clients     map[*WebSocketClient]bool //todo хранить соединения в редисе
+	Messagebox  chan []*Message
+	Add         chan *WebSocketClient
+	Delete      chan *WebSocketClient
+	Subscribe   chan *Subscribe
+	Unsubscribe chan *Subscribe
+}
+
+func NewWebSocketHub(app *settings.App) *WebSocketHub {
+	return &WebSocketHub{
+		App:         app,
+		Mx:          &sync.RWMutex{},
+		Clients:     make(map[*WebSocketClient]bool),
+		Messagebox:  make(chan []*Message),
+		Add:         make(chan *WebSocketClient),
+		Delete:      make(chan *WebSocketClient),
+		Subscribe:   make(chan *Subscribe),
+		Unsubscribe: make(chan *Subscribe),
+	}
+}
+
+type Subscribe struct {
+	Client *WebSocketClient
+	ChatId int64
 }
